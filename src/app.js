5dc809e10731b8594c509afcae2c040cc5fd939b
lydiajagdeo <lydiajagdeo@gmail.com>
2022-05-31 14:31:45 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["The", "A"];
+  let subject = ["tree", "child", "squirrel", "cat"];
+  let action = ["cried on my", "danced with my", "tore my", "withered my"];
+  let possession = ["hair", "phone", "clothes", "shoes"];
+  let where = ["garden", "forest", "river", "desert"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return;
+  pronoun[pronounIndex] +
+    "" +
+    subject[subjectIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    possession[possessionIndex] +
+    "" +
+    where[whereIndex];
 };
